127af35b48777db77915a042a9a3e5d0539c3230
S4da <132928844+Noemi-sada@users.noreply.github.com>
2023-05-29 08:01:59 -0600

    Add files via upload

---
+++ 2905sada.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+	const int luna= 238857;
+	float luna_kilo; //distancia en millas
+	printf("distancia a la luna %d mullas\n", luna);
+	luna_kilo=luna*1.609; /*una milla es igual a 1.609 kilometro
+	printf("en kilometros es %.2km. \n", luna_kilo);
+	return 0
+	;
+}
---
+++ dnsc2905.cpp
@@ -0,0 +1,22 @@
+/*programa numero
+  titulo:
+  fecha:
+  Nombre: Rivelino Reyes Gonzalez*/
+  #include <stdio.h>
+  int n;
+  int main()
+  int j;
+  {
+  	printf( "¿Cual es su numero favorito? ");
+  	scanf("%d", &n);
+  	int ni=10;
+  	int i;
+  	for(i=2; i<ni; i=i+2){
+  	printf("interaccion numero: %d\n",i);
+	  }
+	printf("cuanto vale aqui la variable i: %d\n",i);
+	printf("cuanto vale aqui la variable n: %d\n",n);
+	printf("cuanto vale aqui la variable ni: %d\n",ni);
+	
+	return 0;
+	  }	
