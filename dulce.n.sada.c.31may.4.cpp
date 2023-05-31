476c6f5926f2d0473f1a58e994695e8f2832c7ec
S4da <132928844+Noemi-sada@users.noreply.github.com>
2023-05-31 08:37:10 -0600

    Add files via upload

---
+++ dulce.n.sada.c.31may.4.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	int m=99,n;
+	n= ++m;
+	
+	printf("m= %d, n=%d\n",m,n);
+	n= ++m;
+	printf("m= %d, n=%d\n",m,n);
+	printf("m= %d\n",m++);
+	printf("m= %d\n",++m);
+}
---
+++ i.op.2.31.mayo.sada.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	int n=5,t;
+	t= ++n*--n;
+
+	
+	printf("n= %d, t=%d\n",n,t);
+	printf("%d %d %d\n",++n,++n,++n);
+}
