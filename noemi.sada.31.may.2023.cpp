f009fa3143748933995b17fe3a66f640657d4d8d
S4da <132928844+Noemi-sada@users.noreply.github.com>
2023-05-31 07:49:11 -0600

    Add files via upload

---
+++ noemi.sada.31.may.2023.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	printf("el tamaño de variables de como flotante es %d",sizeof (float));
+	printf("bytes\n");
+	printf("el tamaño de variables de doble precision es %d",sizeof (double));
+	printf("bytes\n");
+	return 0;
+}
