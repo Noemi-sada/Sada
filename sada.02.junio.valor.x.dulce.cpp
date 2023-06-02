6ce4cadb7367dae2dcb7ca0edcf997865da6ab1d
S4da <132928844+Noemi-sada@users.noreply.github.com>
2023-06-02 10:27:51 -0600

    Add files via upload

---
+++ sada.02.junio.valor.x.dulce.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float f,x;
+	printf ("\n elige un valor de x");
+	scanf ("%f",&x);
+	/*seleccion de rango de x*/
+	if (x<= 0.0){
+	    f= -pow(x,2) -x;}
+	else{
+	    f= -pow(x,2) +3*x;
+	printf("f(%.1f) = %.3f",x,f);}
+	return 0;    
+}
